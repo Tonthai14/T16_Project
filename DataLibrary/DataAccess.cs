2aad99a40e362e8ee38c7b6ac5b8708fd0ecdb96
Tonthai14 <Tonthai14@gmail.com>
2021-04-02 18:25:30 -0500

    first commit

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Dapper;
+using System.Linq;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
---
+++ Team16Project/Shared/UserResponseManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team16Project.Shared
+{
+    public class UserResponseManager
+    {
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public string[] Errors { get; set; }
+        public Dictionary<string, string> UserInfo { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+    }
+}
