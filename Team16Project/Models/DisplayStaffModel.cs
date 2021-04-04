452a1b5afc39dc807502ca8dc2423f1a7766db2f
Tonthai14 <Tonthai14@gmail.com>
2021-04-04 18:05:48 -0500

    Updated folders

---
+++ Team16Project/Models/DisplayStaffModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team16Project.Models
+{
+    public class DisplayStaffModel
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "First name is too long.")]
+        [MinLength(2, ErrorMessage = "First name is too short.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Last name is too long.")]
+        [MinLength(2, ErrorMessage = "Last name is too short.")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Job { get; set; }
+    }
+}
