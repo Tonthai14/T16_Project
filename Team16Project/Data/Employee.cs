c4e174bcf4cd755fd5afbaa0aba9876446df2d7e
Tonthai14 <Tonthai14@gmail.com>
2021-04-04 18:32:23 -0500

    Updated data

---
+++ Team16Project/Data/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team16Project.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public Job job { get; set; }
+    }
+}
---
+++ Team16Project/Data/Job.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team16Project.Data
+{
+    public class Job
+    {
+        public short JobId { get; set; }
+        public string JobDesc { get; set; }
+    }
+}
