416ba2a2058feba4b92bfb07659d1bae9102da2c
ZoroXF <jonathanmena123@yahoo.com>
2021-04-05 01:33:22 -0500

    Create employee rides

---
+++ DataLibrary/Models/RidesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class RidesModel
+    {
+        public int ride_id { get; set; }
+        public int times_ridden { get; set; }
+        public int breakdowns { get; set; }
+        public string tickets_to_ride { get; set; }
+        public string tickets_used { get; set; }
+        public string ride_name { get; set; }
+
+    }
+}
---
+++ Team16Project/Models/DisplayEmployeeRidesModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team16Project.Models
+{
+    public class DisplayEmployeeRidesModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Name of ride too long.")]
+        [MinLength(2, ErrorMessage = "Name of ride too short.")]
+        public string RideName { get; set; }
+
+        [Required]
+        public string TimesRidden { get; set; }
+
+    }
+}
